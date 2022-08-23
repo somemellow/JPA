297206de53fda2a0f94e53393320f107ffd5a382
h5nname <somemellow74@gmail.com>
2022-08-23 23:50:27 +0900

    item Repository, Service Create

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+    /**
+     *  stock 감소
+     */
+    public void removeStock(int quantity){
+        int resetStock = this.stockQuantity - quantity;
+        if(resetStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=resetStock;
+    }
 }
