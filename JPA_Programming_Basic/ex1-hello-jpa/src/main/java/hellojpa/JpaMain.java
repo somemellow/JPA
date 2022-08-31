dee48b6ec5091d322a675a7b15be3345a84d486c
h5nname <somemellow74@gmail.com>
2022-08-31 14:34:40 +0900

    database Search

--- JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,33 +12,9 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            //Code 저장
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-//            em.persist(member);
-            //조회 Code
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
-            //삭제 Code
-//            Member findMember = em.find(Member.class, 1L);
-//            em.remove(findMember);
-            //수정 Code
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA");
-            //JPQL(객체지향 SQL)
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== BEFORE ===");
 
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
             tx.commit();    
         }catch(Exception e){
