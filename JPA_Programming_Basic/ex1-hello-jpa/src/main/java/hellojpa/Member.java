24e3868590a3d8d2b2fa20d9c4a8fe5cb4e65266
h5nname <somemellow74@gmail.com>
2022-09-01 11:53:50 +0900

    DATABASE SCHEMA AUTO CREATE

--- JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,8 +1,6 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "MBR")
@@ -15,6 +13,7 @@ public class Member {
 
     @Id
     private Long id;
+    @Column(unique = true, length = 10)
     private String name;
 
     public Long getId() {
