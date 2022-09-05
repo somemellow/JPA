639e0560c8b89e48edf664f97e048ad282955a16
h5nname <somemellow74@gmail.com>
2022-09-05 16:53:29 +0900

    multy relationship mapping  Example

--- JPA_Programming_Basic/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA_Programming_Basic/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,16 +1,62 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Order {
     @Id @GeneratedValue
     @Column(name ="ORDER_ID")
     private Long Id;
-    @Column(name="MEMBER_ID")
-    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name="MEMBER_ID")
+    private Member member;
+
     private Date orderDate;
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
