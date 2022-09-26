a2d434484d98152eaffb69ede84bbd72a9cee71f
somemellow <somemellow74@gmail.com>
2022-09-26 16:19:10 +0900

    Querydsl Pracitical use - Controller Develop

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,13 +2,16 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -89,18 +92,19 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
-         long total = queryFactory
-                 .select(member)
-                 .from(member)
-                 .leftJoin(member.team, team)
-                 .where(
-                         usernameEq(condition.getUsername()),
-                         teamnameEq(condition.getTeamName()),
-                         ageGoe(condition.getAgeGoe()),
-                         ageLoe(condition.getAgeLoe())
-                 )
-                 .fetchCount();
-        return new PageImpl<>(content, pageable, total);
+
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamnameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                );
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+        //return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression ageBetween(int ageLoe, int ageGoe){
