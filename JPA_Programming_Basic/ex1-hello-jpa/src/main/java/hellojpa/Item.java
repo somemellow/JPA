777dd6fc6d198d92ffaeef18a2eb5d194bbb4851
somemellow <somemellow74@gmail.com>
2022-09-28 11:44:53 +0900

    Java ORM Standard JPA Programming Basic - Advanced Mapping Inheritance Mapping Join Strategies

---
+++ JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Item {
+    @Id @GeneratedValue
+    private Long Id;
+    private String name;
+    private int price;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
