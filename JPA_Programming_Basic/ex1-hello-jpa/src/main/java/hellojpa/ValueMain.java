43a92a09ea3e85f28a08d980987c77bf4cf8468c
somemellow <somemellow74@gmail.com>
2022-10-05 16:51:13 +0900

    Java ORM Standard JPA Programming Basic - value type compare

--- JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ JPA_Programming_Basic/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -3,13 +3,10 @@ package hellojpa;
 public class ValueMain {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = a;
+        Address address1 = new Address("city", "street", "zipcode");
+        Address address2 = new Address("city", "street", "zipcode");
 
-        a = 20;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("address1.equals(address2) = " + address1.equals(address2));
 
     }
 }
