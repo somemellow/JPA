a2f28c2a9fd014d0a66809564d5cdad23d2d77c5
somemellow <somemellow74@gmail.com>
2022-10-08 16:21:17 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language TypeQuery and Query

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpaMain {
@@ -16,7 +13,12 @@ public class JpaMain {
         try{
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username,m.age from Member m");
 
             tx.commit();
         }catch(Exception e){
