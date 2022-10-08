ebbf7fda4fd1100c144be9c822c9d28d63f4cb1b
somemellow <somemellow74@gmail.com>
2022-10-08 17:29:44 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language Projection Paging API

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -11,21 +11,25 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
 
+            for(int i=0; i<100; i++){
+                Member member = new Member();
+                member.setUsername("member"+i);
+                member.setAge(i);
+                em.persist(member);
+            }
+            
             em.flush();
             em.clear();
 
-            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m")
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
                     .getResultList();
-
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
-
+            System.out.println("result.size() = " + result.size());
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
             tx.commit();
         }catch(Exception e){
             tx.rollback();
