16d18e283859032671cd46ce57e37414b88ec1ad
somemellow <somemellow74@gmail.com>
2022-10-08 17:12:19 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language Projection Many Value Search 3nd Way New DTO

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -19,11 +19,12 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
-                     .getResultList();
-            Object[] result = resultList.get(0);
-            System.out.println("username = " + result[0]);
-            System.out.println("age = " + result[1]);
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m")
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
 
             tx.commit();
         }catch(Exception e){
