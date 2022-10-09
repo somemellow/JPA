c88cc964b3d092d6cb21fc61883eacba67b0d268
somemellow <somemellow74@gmail.com>
2022-10-09 19:32:58 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language JPQL User Definition Function

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -11,27 +11,22 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername(null);
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
-            member.setTeam(team);
-            em.persist(member);
-
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member m ";
+            String query = "select function('group_concat', m.username) from Member m ";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (String s : result) {
+            for (String  s : result) {
                 System.out.println("s = " + s);
             }
 
