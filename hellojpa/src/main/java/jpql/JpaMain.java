2e36da599f3ca07fe5df1ea4f8dba9c8171920a8
somemellow <somemellow74@gmail.com>
2022-10-09 17:49:38 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language Projection Not Have Relationship Entity outer join

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -26,7 +26,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m LEFT JOIN m.team t on t.name = 'teamA'";
+            String query = "select m from Member m left join Team t on m.username = t.name";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
