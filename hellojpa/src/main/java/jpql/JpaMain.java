61539bec7427e286c637feeb2f561f63b9c27df1
somemellow <somemellow74@gmail.com>
2022-10-09 19:04:08 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language JPQL ETC CASE coalesce

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -16,7 +16,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername(null);
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
@@ -27,13 +27,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query =
-                    "select " +
-                            "case when m.age <= 10 then '학생요금'" +
-                            "     when m.age >= 60 then '경로요금'" +
-                            "     else '일반요금'" +
-                            "end " +
-                    "from Member m";
+            String query = "select coalesce(m.username, '이름 없는 회원') from Member m ";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
