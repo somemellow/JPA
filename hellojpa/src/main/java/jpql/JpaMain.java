95f20d93a9a2d59c1e1dc4f0833b9f36c4448d89
somemellow <somemellow74@gmail.com>
2022-10-11 16:32:04 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language JPQL Biggner Grammar Path Expression

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -11,23 +12,28 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat', m.username) from Member m ";
-            List<String> result = em.createQuery(query, String.class)
+            String query = "select t.members from Team t ";
+            Collection result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (String  s : result) {
-                System.out.println("s = " + s);
+            for (Object o : result) {
+                System.out.println("o = " + o);
             }
 
             tx.commit();
