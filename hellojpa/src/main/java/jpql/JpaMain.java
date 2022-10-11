52034f83aae30bab8bca574b00a1da9827faa5a5
somemellow <somemellow74@gmail.com>
2022-10-11 17:42:50 +0900

    Java ORM Standard JPA Programming Basic - Object Oriented Query Language JPQL Biggner Grammar fetch join Characteristic and Limit

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -38,9 +38,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t from Team t join t.members";
+            String query = "select t from Team t";
             List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
+
+            System.out.println("result.size() = " + result.size());
 
             for (Team team : result) {
                 System.out.println("team = " + team.getName() + "|members = "+ team.getMembers().size());
